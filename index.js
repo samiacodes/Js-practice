640585c55dcff76b5a1783e14e23c21872eb2e6f
samiacodes <emiliyalamia@gmail.com>
2025-08-31 20:54:51 +0600

    Initial commit: loops practice with forEach and map

---
+++ index.js
@@ -0,0 +1,37 @@
+// find even odd usinf forEach
+// problem is it can not stop loop by break or return 
+
+const Numbers = [1,2,3,4,5,6,7,8,90,100,200,330,400,500,606,603];
+let sum = 0;
+const names =["zara","sara","lira","kira","mira","nira"];
+const Uppercase = []
+// Numbers.forEach(num => {
+//     if(num%2===0){
+//         return console.log(`${num}-is even`);
+//     }
+//     else{
+//         return console.log(`${num}-is odd`);
+//     }
+// })
+// Pro version
+Numbers.forEach(num=> {
+    const type = num%2===0?"even":"odd";
+    console.log(`${num}- is ${type}`);
+})
+
+// let's sum of numbers
+
+Numbers.forEach(num => {
+   sum += num;
+});
+console.log(`Total sum is ${sum}`);
+
+// uppercase all names 
+names.forEach(name => {
+    Uppercase.push(name.toUpperCase());
+});
+
+console.log(Uppercase);
+
+const MapLowerCase = Uppercase.map(name=> name.toLowerCase());
+console.log(MapLowerCase,"Using Map");
